509ac171146ed59e648668ef15ee645b6ad97755
Malcolm Boyes <38740057+boyesm@users.noreply.github.com>
2022-03-13 17:03:47 -0400

    created python function to talk with firmware

---
+++ comms.py
@@ -0,0 +1,13 @@
+import serial
+import struct
+
+arduino = serial.Serial()
+arduino.port = 'COM4'  # TODO: ensure this is the correct port for the arduino
+arduino.baudrate=9600
+arduino.timeout=0.1
+arduino.open()
+
+# this will send a signal to the arduino to turn the lights on for a few seconds
+def turn_on_lights():
+    arduino.write(struct.pack('>2B', int(1), int(0)))
+
